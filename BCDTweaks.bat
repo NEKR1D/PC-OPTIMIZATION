f1c4faa5f1b880e65e3cd7101fb5245ded614d0d
L O G A N <154441675+NEKR1D@users.noreply.github.com>
2025-07-13 05:38:08 -0700

    Update BCDTweaks.bat

--- BCDTweaks.bat
+++ BCDTweaks.bat
@@ -1,7 +1,7 @@
-rem # BCDEDIT Tweaks
+rem ::: BCDEDIT Tweaks
 
-rem # Plundered and updated by NEKR1D
-rem # Originally created by Shoober420
+rem ::: Plundered and updated by NEKR1D
+rem ::: Originally created by Shoober420
 
 if not exist C:\Windows\System32\wbem\WMIC.exe (
     echo Installing WMIC...
@@ -9,24 +9,24 @@ if not exist C:\Windows\System32\wbem\WMIC.exe (
     echo Done.
 )
 
-rem # Processor x2APIC Support helps operating systems run more efficiently on high core count configurations
+rem ::: Processor x2APIC Support helps operating systems run more efficiently on high core count configurations
 bcdedit /set x2apicpolicy Enable
 
-rem # Enable MSI
+rem ::: Enable MSI
 bcdedit /set configaccesspolicy Default
 bcdedit /set MSI Default
 bcdedit /set usephysicaldestination No
 bcdedit /set usefirmwarepcisettings No
 
-rem # Avoid the use of uncontiguous portions of low-memory from the OS
+rem ::: Avoid the use of uncontiguous portions of low-memory from the OS
 bcdedit /set firstmegabytepolicy UseAll
 bcdedit /set avoidlowmemory 0x8000000
 bcdedit /set nolowmem Yes
 
-rem # Enable Physical Address Extension (PAE)
+rem ::: Enable Physical Address Extension (PAE)
 bcdedit /set pae ForceEnable
 
-rem # Disable 57-bits 5-level paging, also known as "Linear Address 57". Only 100% effective on 10th gen Intel. 256 TB of virtual memory per-disk is way much more than enough anyway
+rem ::: Disable 57-bits 5-level paging, also known as "Linear Address 57". Only 100% effective on 10th gen Intel. 256 TB of virtual memory per-disk is way much more than enough anyway
 bcdedit /set linearaddress57 OptOut
 bcdedit /set increaseuserva 268435328
 
@@ -52,9 +52,9 @@ bcdedit /set halbreakpoint No
 bcdedit /set bootmenupolicy Legacy
 bcdedit /set graphicsmodedisabled No
 
-rem # Set Number of Processors 
-rem # define processor count (includes counting cores with HT on as 2 processors example; 8 cores = 16 processor threads if HT is on)
-rem # intel 12900K with HT disabled = 16 cores/processors (8 P-core and 8 E-core)
+rem ::: Set Number of Processors 
+rem ::: define processor count (includes counting cores with HT on as 2 processors example; 8 cores = 16 processor threads if HT is on)
+rem ::: intel 12900K with HT disabled = 16 cores/processors (8 P-core and 8 E-core)
 bcdedit /set numproc 16
 
 PAUSE
