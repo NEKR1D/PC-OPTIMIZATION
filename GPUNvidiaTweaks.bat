3523f2e1515eb0fc413cb44537f36675fa882fb5
L O G A N <154441675+NEKR1D@users.noreply.github.com>
2025-07-13 05:31:48 -0700

    Update GPUNvidiaTweaks.bat

--- GPUNvidiaTweaks.bat
+++ GPUNvidiaTweaks.bat
@@ -20,12 +20,6 @@ rem # IMPROVE LATENCY AT THE COST OF LITTLE LESS FPS
 
 PAUSE
 
-rem # Sources:
-rem # https://sites.google.com/view/melodystweaks/basictweaks
-rem # https://github.com/sherifmagdy32/gaming_os_tweaker
-
-rem # https://www.elevenforum.com/members/garlin.5387/
-rem # https://www.elevenforum.com/t/gpu-tweaks-batch-script.30134/post-521530
 if not exist C:\Windows\System32\wbem\WMIC.exe (
     echo Installing WMIC...
     DISM /Online /Add-Capability /CapabilityName:WMIC~~~~
