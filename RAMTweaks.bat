8333bcf33bb603537d9198c109c6561b5ece2a67
L O G A N <154441675+NEKR1D@users.noreply.github.com>
2025-07-13 05:27:49 -0700

    Update RAMTweaks.bat

--- RAMTweaks.bat
+++ RAMTweaks.bat
@@ -147,25 +147,3 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management
 PowerShell -Command "Disable-MMAgent -PageCombining"
 
 PAUSE
-
-rem # SvcHostSplitThresholdInKB value is amount of RAM in KiloBytes (KB)
-rem # Example math formula: 8 GB = 8x1024 MB = 8x1024x1024 KB = 8388608 KB
-rem # 8GB = 8388608 (800000) / 16GB = 16777216 (1000000) / 32GB = 33554432 (2000000) / 64GB = 67108864 (4000000)
-
-rem # IOPageLockLimit will limit RAM to use certain tasks
-rem # Example math formula: 8 GB = 8x1024 MB = 8x1024x1024 KB = 8388608 KB
-rem # 8GB = 8388608 (800000) / 16GB = 16777216 (1000000) / 32GB = 33554432 (2000000) / 64GB = 67108864 (4000000)
-
-rem # CacheUnmapBehindLengthInMB trims your massive WorkingSet
-
-rem # https://www.tenforums.com/performance-maintenance/189717-test-trim-workingset-ram.html
-
-rem # https://www.i.u-tokyo.ac.jp/edu/training/ss/lecture/new-documents/Lectures/15-CacheManager/CacheManager.pdf
-
-rem # CacheUnmapBehindLengthInMB=dword:00000100 ;256MB = 4GB RAM / 512MB = 8GB RAM / 1024MB = 16GB RAM / 2048MB = 32GB RAM / 4096MB = 64GB RAM
-rem # dword:00000100 = 256MB = 4GB RAM / dword:00000200 = 512MB = 8GB RAM / dword:00000300 = 768MB = 12GB RAM / dword:00000400 = 1024MB = 16GB RAM / dword:00000800 = 2048MB = 32GB RAM / dword:00001600 = 4096MB = 64GB RAM
-
-
-rem # ModifiedWriteMaximum=dword:00000020 ;32MB = 4GB RAM / 64MB = 8GB RAM / 128MB = 16GB RAM / 256MB = 32GB RAM / 512MB = 64GB RAM
-
-rem # dword:00000020 ;32MB = 4GB RAM / dword:00000040 ;64MB = 8GB RAM / dword:00000060 ;96MB = 12GB RAM / dword:00000080 ;128MB = 16GB RAM / dword:00000160 ;256MB = 32GB RAM / dword:00000320 ;512MB = 64GB RAM
