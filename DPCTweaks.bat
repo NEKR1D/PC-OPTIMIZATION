a43a642e555aa09d280ca973430be2d4b63f1653
L O G A N <154441675+NEKR1D@users.noreply.github.com>
2025-07-13 05:35:22 -0700

    Update DPCTweaks.bat

--- DPCTweaks.bat
+++ DPCTweaks.bat
@@ -1,16 +1,9 @@
-rem # DPC Tweaks
+rem # Deferred Procedure Call (DPC) Tweaks
 
-rem # Deferred Procedure Call (DPC)
-rem # Allows high-priority tasks to defer required but lower-priority tasks for later execution
+rem # Plundered by NEKR1D
 
-rem # Thread DPC
-rem # Prioritizes critial tasks to execute first
-
-rem # https://forums.blurbusters.com/viewtopic.php?t=13323
-rem # https://www.tenforums.com/performance-maintenance/146774-cacheawarescheduling-what-how-does-windows-benefit.html
-
-@ECHO off
-ECHO Adding values to the Registry...
+@echo off
+echo Adding values to the Registry...
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v DpcWatchdogProfileOffset /t REG_DWORD /d 0 /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v DpcTimeout /t REG_DWORD /d 0 /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v IdealDpcRate /t REG_DWORD /d 1 /f
@@ -22,6 +15,6 @@ reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\ker
 rem # Disable Threaded DPCs
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "ThreadDpcEnable" /t REG_DWORD /d "0" /f
 
-ECHO Completed adding values to the Registry.
+echo Completed adding values to the Registry.
 
 PAUSE
