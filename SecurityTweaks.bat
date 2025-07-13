115368ab30d1342008b05f33f9dc23650566a020
L O G A N <154441675+NEKR1D@users.noreply.github.com>
2025-07-13 05:44:22 -0700

    Update SecurityTweaks.bat

--- SecurityTweaks.bat
+++ SecurityTweaks.bat
@@ -1,26 +1,26 @@
-rem # Disable CPU Mitigations for Spectre and Meltdown attacks
+rem ::: Disable CPU Mitigations for Spectre and Meltdown attacks
 
-rem # Plundered and updated by NEKR1D
-rem # Originally created by Shoober420
+rem ::: Plundered and updated by NEKR1D
+rem ::: Originally created by Shoober420
 
-rem # !!! WARNING !!!
+rem ::: !!! WARNING !!!
 
-rem # SCRIPT FOR REFERENCE ONLY
-rem # NOT RECOMENDED (BUT HAS POTENTIAL TO INCEASE YOUR CPU PERFORMANCE)
-rem # USE ON GAMING IMAGE ONLY, DONT DO SENSITIVE TASKS SUCH AS BANKING
+rem ::: SCRIPT FOR REFERENCE ONLY
+rem ::: NOT RECOMENDED (BUT HAS POTENTIAL TO INCEASE YOUR CPU PERFORMANCE)
+rem ::: USE ON GAMING IMAGE ONLY, DONT DO SENSITIVE TASKS SUCH AS BANKING
 
-rem # !!! WARNING !!!
+rem ::: !!! WARNING !!!
 
 PAUSE
 
-rem # 0/3 - Enable / 3/3 - Disable mitigations for CVE-2017-5715 (Spectre Variant 2) and CVE-2017-5754 (Meltdown)
+rem ::: 0/3 - Enable / 3/3 - Disable mitigations for CVE-2017-5715 (Spectre Variant 2) and CVE-2017-5754 (Meltdown)
 
 reg add "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettingsOverride" /t REG_DWORD /d "3" /f
 reg add "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettingsOverrideMask" /t REG_DWORD /d "3" /f
 reg add "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettings" /t REG_DWORD /d "3" /f
 
-rem # 0 - Disable 1 = Enable - Windows Security Control Flow Guard
-rem # https://learn.microsoft.com/en-us/windows/win32/secbp/control-flow-guard
+rem ::: 0 - Disable 1 = Enable - Windows Security Control Flow Guard
+rem ::: https://learn.microsoft.com/en-us/windows/win32/secbp/control-flow-guard
 reg add "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management" /v "EnableCfg" /t REG_DWORD /d "0" /f
 
 PAUSE
