ef94ccef502bbbce5ab45d52341ce1b0cc53cb01
L O G A N <154441675+NEKR1D@users.noreply.github.com>
2025-07-13 18:53:16 -0700

    Update NetworkTweaks.bat

--- NetworkTweaks.bat
+++ NetworkTweaks.bat
@@ -1,108 +1,111 @@
-rem ::: Network Tweaks
+rem ::: Network Tweaks 
 
 rem ::: Plundered and updated by NEKR1D
 rem ::: Originally created by Shoober420
 
 rem ::: !!! Warning !!!
 rem ::: !!! Your hardware and devices are different !!!
-rem ::: !!! May brick your NIC connectivity !!!
+rem ::: !!! May brick your Network Adapter and connectivity !!!
 rem ::: !!! Use script as reference only !!!
 
+rem ::: !!! Setting things at tcp stack, device or registry level can have unintended behavior if they are mismatched !!!
+rem ::: !!! Some settings could take priority over the other !!!
+
 if not exist C:\Windows\System32\wbem\WMIC.exe (
-    echo Installing WMIC...
+    rem :::Installing WMIC...
     DISM /Online /Add-Capability /CapabilityName:WMIC~~~~
-    echo Done.
+    rem :::Done.
 )
 
 TIMEOUT 1
 
 @echo off
-echo.
-echo Starting Network Tweaks Script...
-echo.
 
-echo Setting DNS as Cloudflare 1.1.1.1 / 1.0.0.1
+rem ::: Windows Network and Internet Connectivity Tweaks
+
+rem :::Setting DNS as Cloudflare 1.1.1.1 / 1.0.0.1
 netsh interface ipv4 set dnsservers name="Ethernet" static 1.1.1.1 primary
-netsh interface ipv4 add dnsservers name="Ethernet" address=1.0.0.1 index=2
+netsh interface ipv4 set dnsservers name="Ethernet" 1.0.0.1 index=2
 
-echo Enabling DNS over HTTPS (DoH)
+rem :::Enabling DNS over HTTPS (DoH)
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Dnscache\Parameters" /v "EnableAutoDoh" /t REG_DWORD /d "2" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Dnscache\Parameters" /v "EnableDoh" /t REG_DWORD /d "2" /f
 
 rem ::: Require DoH / 2 - Allow DoH / 1 - Prohibit DoH
-echo Forcing requirement of DoH
 reg add "HKLM\Software\Policies\Microsoft\Windows NT\DNSClient" /v "DoHPolicy" /t REG_DWORD /d "3" /f
 
-echo Disable TCP/IP NetBIOS Helper (lmhosts)
+rem :::Disable TCP/IP NetBIOS Helper (lmhosts)
 net stop lmhosts
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\lmhosts" /v "Start" /t REG_DWORD /d "4" /f
 
-echo "Setting default TTL value in TCP/IP parameters to 64."
+rem :::"Setting default TTL value in TCP/IP parameters to 64."
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /t REG_DWORD /v DefaultTTL /d 64 /f
 
-echo "Setting maximum user port number in TCP/IP parameters to 65534."
+rem :::"Setting maximum user port number in TCP/IP parameters to 65534."
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /t REG_DWORD /v MaxUserPort /d 65534 /f
 
-echo "Setting TCP timed wait delay in TCP/IP parameters to 30 seconds."
+rem :::"Setting TCP timed wait delay in TCP/IP parameters to 30 seconds."
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /t REG_DWORD /v TcpTimedWaitDelay /d 30 /f
 
-echo "Disabling non-best effort bandwidth limit in QoS policies."
+rem :::"Disabling non-best effort bandwidth limit in QoS policies."
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Psched" /t REG_DWORD /v NonBestEffortLimit /d 0 /f
 
-echo "Configuring QoS to not use Network Layer Authentication."
+rem :::"Configuring QoS to not use Network Layer Authentication."
 reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\Tcpip\QoS" /t REG_SZ /v "Do not use NLA" /d 1 /f
 
-echo "Adjusting LanmanServer parameters for optimized file sharing performance."
+rem :::"Adjusting LanmanServer parameters for optimized file sharing performance."
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" /t REG_DWORD /v Size /d 3 /f
 
 rem ::: Network Throttle // Default = 10 Unthrottled = 4294967295 [Decimal Values]
-echo "Setting network throttling index in multimedia system profile"
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /t REG_DWORD /v NetworkThrottlingIndex /d 4294967295 /f
 
-rem ::: Set system responsiveness
-echo "Setting system responsiveness to maximum in multimedia system profile."
+rem :::"Setting system responsiveness to maximum in multimedia system profile."
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /T REG_DWORD /v SystemResponsiveness /d 0 /f
 
-echo "Applying NSI registry settings for network performance optimization."
+rem :::"Applying NSI registry settings for network performance optimization."
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Nsi\{eb004a03-9b1a-11d4-9123-0050047759bc}\26" /v "00000000" /t REG_BINARY /d "0000000000000000000000000500000000000000000000000000000000000000ff00000000000000" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Nsi\{eb004a03-9b1a-11d4-9123-0050047759bc}\26" /v "04000000" /t REG_BINARY /d "0000000000000000000000000500000000000000000000000000000000000000ff00000000000000" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Nsi\{eb004a03-9b1a-11d4-9123-0050047759bc}\0" /v "0200" /t REG_BINARY /d "0000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000ff000000000000000000000000000000000000000000ff000000000000000000000000000000" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Nsi\{eb004a03-9b1a-11d4-9123-0050047759bc}\0" /v "1700" /t REG_BINARY /d "0000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000ff000000000000000000000000000000000000000000ff000000000000000000000000000000" /f
 
-echo "Setting priority levels for DNS, Hosts, Local, and NetBT services."
+rem :::"Setting priority levels for DNS, Hosts, Local, and NetBT services."
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\ServiceProvider" /v "DnsPriority" /t REG_DWORD /d "6" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\ServiceProvider" /v "HostsPriority" /t REG_DWORD /d "5" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\ServiceProvider" /v "LocalPriority" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\ServiceProvider" /v "NetbtPriority" /t REG_DWORD /d "7" /f
 
-echo "Setting IRPStackSize for LanmanServer to enhance file sharing capabilities."
+rem :::"Setting IRPStackSize for LanmanServer to enhance file sharing capabilities."
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" /t REG_DWORD /v IRPStackSize /d 30 /f
 
-echo Setting Packet Scheduler Timer Resolution = 1
+rem :::Setting Packet Scheduler Timer Resolution = 1
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Psched" /v "TimerResolution" /t REG_DWORD /d "1" /f
 
-echo Setting Packet Scheduler - Limit outstanding packets = 0
+rem :::Setting Packet Scheduler - Limit outstanding packets = 0
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Psched" /v "MaxOutstandingSends" /t REG_DWORD /d "0" /f
 
-echo Setting Packet Scheduler - Limit reservable bandwidth = 0
+rem :::Setting Packet Scheduler - Limit reservable bandwidth = 0
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Psched" /v "NonBestEffortLimit" /t REG_DWORD /d "0" /f
+
+rem ::: Network Interface Card [Network Adapter] Tweaks
 
 rem ::: Get the Sub ID of the Network Adapter
 
 for /f %%n in ('Reg query "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002bE10318}" /v "*SpeedDuplex" /s ^| findstr  "HKEY"') do (
 
-rem ::: Speed & Duplex must be set to "Auto Negotiation" or internet borks unless you know correct value [6 = 2.5 Gbit]
+rem ::: Setting Network Adapter SpeedDuplex to 2.5 Gbit [1Gbit = 6] [2.5Gbit = 24]
+rem ::: Speed & Duplex must be set to "Auto Negotiation" or internet breaks unless you know the correct value
+reg add "%%n" /v "*SpeedDuplex" /t REG_SZ /d "24" /f
 
-echo Setting NIC SpeedDuplex to 2.5 Gbit [1Gbit = 6]
-reg add "%%n" /v "*SpeedDuplex" /t REG_SZ /d "7" /f
-
-rem ::: MIMO Power Save Mode - 3 Disable
-echo Disabling MIMO Power Save Mode
+rem ::: Disabling MIMO Power Save Mode -  Disable = 1
 reg add "%%n" /v "MIMOPowerSaveMode" /t REG_SZ /d "3" /f
 
-rem ::: Disable most offloading settings on NIC [visible in device manager]
-echo Disabling NIC offloading, rss, wake-on-LAN, mircast, etc.
+rem ::: Disable most properties/services on Network Adapter 
+rem ::: These are also visible in device manager properties of device
+rem ::: Intel i-225v no longer supports RSS [officially not included in driver or .inf]
+
+rem ::: Disabling Network Adapter offloading, rss, wake-on-LAN, mircast, etc.
 reg add "%%n" /v "*WakeOnMagicPacket" /t REG_SZ /d "0" /f
+reg add "%%n" /v "*WakeOnMagicPacketFromS5" /t REG_SZ /d "0" /f
 reg add "%%n" /v "*WakeOnPattern" /t REG_SZ /d "0" /f
 reg add "%%n" /v "*PacketCoalescing" /t REG_SZ /d "0" /f
 reg add "%%n" /v "ThroughputBoosterEnabled" /t REG_SZ /d "1" /f
@@ -131,24 +134,23 @@ reg add "%%n" /v "*PMARPOffload" /t REG_SZ /d "0" /f
 reg add "%%n" /v "Downshift" /t REG_SZ /d "0" /f
 reg add "%%n" /v "*EEE" /t REG_SZ /d "0" /f
 
-rem ::: Enable Interrupt Moderation on NIC [visible in device manager properties]
+rem ::: Enable Interrupt Moderation on Network Adapter
 rem ::: Set Interrupt Moderation Rate: Interrupt Throttling Rate (ITR) // 125 = Medium 0 = Off
-echo Disabling Interrupt Moderation on NIC [visible in device manager properties]
-reg add "%%n" /v "*InterruptModeration" /t REG_SZ /d "0" /f
-
+reg add "%%n" /v "*InterruptModeration" /t REG_SZ /d "1" /f
 reg add "%%n" /v "ITR" /t REG_SZ /d "125" /f
 
-rem ::: JumboPacket: 1514 = Disabled
-echo Disabling JumboPackets
+rem ::: Disabling JumboPackets: 1514 = Disabled
 reg add "%%n" /v "*JumboPacket" /t REG_SZ /d "1514" /f
 
-rem ::: Disabling NIC QOS, setting buffer sizes = 4096, etc.
-echo Disabling NIC QOS, setting buffer sizes = 4096, etc.
+rem ::: Setting Network buffer sizes = 4096 [Default 1024, 2048]
+
+reg add "%%n" /v "*ReceiveBuffers" /t REG_SZ /d "4096" /f
+reg add "%%n" /v "*TransmitBuffers" /t REG_SZ /d "4096" /f
+
+rem ::: Disabling Network Adapter QOS and other properties
 reg add "%%n" /v "LogLinkStateEvent" /t REG_SZ /d "0" /f
 reg add "%%n" /v "*QoS" /t REG_SZ /d "0" /f
 reg add "%%n" /v "*PriorityVLANTag" /t REG_SZ /d "0" /f
-reg add "%%n" /v "*ReceiveBuffers" /t REG_SZ /d "4096" /f
-reg add "%%n" /v "*TransmitBuffers" /t REG_SZ /d "4096" /f
 reg add "%%n" /v "*RscIPv4" /t REG_SZ /d "0" /f
 reg add "%%n" /v "*RscIPv6" /t REG_SZ /d "0" /f
 reg add "%%n" /v "WakeOnLink" /t REG_SZ /d "0" /f
@@ -165,107 +167,101 @@ rem ::: Disable Power Management options
 reg add "%%n" /v "PnPCapabilities" /t REG_DWORD /d "0x00000118" /f
 )
 
-rem ::: MSI mode support for NIC
+rem ::: MSI mode support for Network Adapter
 for /f %%i in ('wmic path Win32_NetworkAdapter get PNPDeviceID ^| findstr /l "PCI\VEN_"') do (
 	reg add "HKLM\SYSTEM\CurrentControlSet\Enum\%%i\Device Parameters\Interrupt Management\MessageSignaledInterruptProperties" /v "MSISupported" /t REG_DWORD /d "1" /f
 	reg add "HKLM\SYSTEM\CurrentControlSet\Enum\%%i\Device Parameters\Interrupt Management\Affinity Policy" /v "DevicePriority" /t REG_DWORD /d "0" /f
 )
 
-rem ::: NIC Tweaks
-
-rem ::: Last key changes based on NIC card registry ID
+rem ::: Last key changes based on Network Adapter card registry ID
 rem ::: Find "Class Guid" under Device Manager > Network adapters > Properties > Details tab
-rem ::: Go to "HKLM\SYSTEM\CurrentControlSet\Control\Network\"Class Guid"\<NIC ID>\Connection" to get NIC ID
+rem ::: Go to "HKLM\SYSTEM\CurrentControlSet\Control\Network\"Class Guid"\<Network Adapter ID>\Connection" to get Network Adapter ID
 rem ::: https://www.elevenforum.com/members/garlin.5387/
-rem ::: https://www.elevenforum.com/t/find-nic-class-guid-in-batch-script-using-wmic-or-anything-else.30083/
+rem ::: https://www.elevenforum.com/t/find-Network Adapter-class-guid-in-batch-script-using-wmic-or-anything-else.30083/
 
 rem ::: Disable Nagle's Algorithm and NETBIOS
 
-for /f "delims=" %%n in ('wmic nic where "GUID is not null" get guid ^| findstr /v GUID') do (
-   reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\%%n" /v TCPNoDelay /t REG_DWORD /d 1 /f
-   reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\%%n" /v TcpNoDelay /t REG_DWORD /d 1 /f
-   reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\%%n" /v TcpDelAckTicks /t REG_DWORD /d 1 /f
-   reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\%%n" /v TcpAckFrequency /t REG_DWORD /d 1 /f
-   reg add "HKLM\SYSTEM\CurrentControlSet\Services\NetBT\Parameters\Interfaces\Tcpip_%%n" /v NetbiosOptions /t REG_DWORD /d 2 /f
+for /f "delims=" %%n in ('wmic Network Adapter where "GUID is not null" get guid ^| findstr /v GUID') do (
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\%%n" /v TCPNoDelay /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\%%n" /v TcpNoDelay /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\%%n" /v TcpDelAckTicks /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\%%n" /v TcpAckFrequency /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\NetBT\Parameters\Interfaces\Tcpip_%%n" /v NetbiosOptions /t REG_DWORD /d 2 /f
 )
 
-rem ::: Maximum Transmission Unit (MTU)
+rem ::: Maximum Transmission Unit (MTU) Tweaks
 
 echo.
 
-rem ::: FastSendDatagramThreshold / FastCopyReceiveThreshold should match MTU value in decimal, not hexadecimal (usually 1472)
-echo Setting FastSendDatagram to match MTU value of 1500
-echo Setting FastCopyReceiveThreshold to match MTU value of 1500
-echo Disabling MTU Discovery/Auto Mode
+rem ::: FastSendDatagramThreshold / FastCopyReceiveThreshold should match MTU value in decimal (usually 1472), not hexadecimal
+rem ::: Setting FastSendDatagram to match MTU value of 1500
+rem ::: Setting FastCopyReceiveThreshold to match MTU value of 1500
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\AFD\Parameters" /v "FastSendDatagramThreshold" /t REG_DWORD /d 1500 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\AFD\Parameters" /v "FastCopyReceiveThreshold" /t REG_DWORD /d 1500 /f
 
-rem ::: Disables MTU Discovery, which auto sets MTU value randomly based on traffic, never enable
+rem ::: Disabling MTU Discovery/Auto Mode
+rem ::: Disable auto setting MTU value randomly based on traffic
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "EnablePMTUDiscovery" /t REG_DWORD /d "0" /f
 
-rem ::: Set MTU = 1500 on NIC
-echo Setting MTU size to 1500 on NIC
+rem ::: Setting MTU size to 1500 on Network Adapter
 netsh interface ipv4 set subinterface "Ethernet" mtu=1500 store=persistent
 
-rem ::: Setting congestion provider to BBR2 [new option available in Windows 11 is faster]
-echo Setting Internet congestion provider to BBR2
-echo Setting Datacenter congestion provider to BBR2
-echo Setting Compat congestion provider to BBR2
-echo Setting DatacenterCustom congestion provider to BBR2
-echo Setting InternetCustom congestion provider to BBR2
+rem ::: Setting congestion provider to BBR2 
+rem ::: New option available in Windows 11 that is faster
 powershell -Command "netsh int tcp set supplemental Template=Internet CongestionProvider=bbr2" >nul
 powershell -Command "netsh int tcp set supplemental Template=Datacenter CongestionProvider=bbr2" >nul
 powershell -Command "netsh int tcp set supplemental Template=Compat CongestionProvider=bbr2" >nul
 powershell -Command "netsh int tcp set supplemental Template=DatacenterCustom CongestionProvider=bbr2" >nul
 powershell -Command "netsh int tcp set supplemental Template=InternetCustom CongestionProvider=bbr2" >nul
 
-echo Enabling Network Direct Memory Access and Disabling Direct Cache Access, RSS and RSC settings globally.
-powershell -Command "netsh int tcp set global netdma=enabled dca=disabled rss=disabled rsc=disabled" >nul
+rem ::: Enabling Network Direct Memory Access and Disabling Direct Cache Access, RSS and RSC settings globally.
+powershell -Command "netsh int tcp set global netdma=enabled" >nul
+powershell -Command "netsh int tcp set global dca=disabled" >nul
+powershell -Command "netsh int tcp set global rss=disabled" >nul
+powershell -Command "netsh int tcp set global rsc=disabled" >nul
 
-echo Disabling TCP timestamps globally.
+rem ::: Disabling TCP timestamps globally.
 powershell -Command "netsh int tcp set global timestamps=disabled" >nul
 
-echo Setting initial Retransmission Timeout (RTO) to 2000 milliseconds globally.
+rem ::: Setting initial Retransmission Timeout (RTO) to 2000 milliseconds globally.
 powershell -Command "netsh int tcp set global initialRto=2000" >nul
 
-echo Disabling Non-SACK RTT Resiliency globally.
+rem ::: Disabling Non-SACK RTT Resiliency globally.
 powershell -Command "netsh int tcp set global nonsackrttresiliency=disabled" >nul
 
-echo Setting maximum SYN retransmissions to 2 globally.
+rem ::: Setting maximum SYN retransmissions to 2 globally.
 powershell -Command "netsh int tcp set global maxsynretransmissions=2" >nul
 
-echo Setting dynamic port range for TCP on IPv4.
+rem ::: Setting dynamic port range for TCP on IPv4.
 powershell -Command "netsh int ipv4 set dynamicport tcp start=10000 num=3000" >nul
 
-echo Disabling TCP Chimney Offload globally.
+rem ::: Disabling TCP Chimney Offload globally.
 powershell -Command "Set-NetOffloadGlobalSetting -Chimney Disabled" >nul
 
-echo Disabling Checksum Offload on all network adapters.
+rem ::: Disabling Checksum Offload on all network adapters.
 powershell -Command "Disable-NetAdapterChecksumOffload -Name * -Confirm:$false" >nul
 
-echo Disabling Receive-Side Scaling on all network adapters.
+rem ::: Disabling Receive-Side Scaling on all network adapters.
 powershell -Command "Disable-NetAdapterRss -Name * -Confirm:$false" >nul
 
-echo Disabling Receive Segment Coalescing on all network adapters.
+rem ::: Disabling Receive Segment Coalescing on all network adapters.
 powershell -Command "Disable-NetAdapterRsc -Name * -Confirm:$false" >nul
 
-echo Disabling Packet Coalescing Filter globally.
+rem ::: Disabling Packet Coalescing Filter globally.
 powershell -Command "Set-NetOffloadGlobalSetting -PacketCoalescingFilter Disabled" >nul
 
-echo Disabling Large Send Offload on all network adapters.
+rem ::: Disabling Large Send Offload on all network adapters.
 powershell -Command "Disable-NetAdapterLso -Name * -Confirm:$false" >nul
 
-echo Setting minimum RTO (Retransmission Timeout) to 300 milliseconds for InternetCustom profile.
+rem ::: Setting minimum RTO (Retransmission Timeout) to 300 milliseconds for InternetCustom profile.
 powershell -Command "Set-NetTCPSetting -SettingName InternetCustom -MinRto 300" >nul
 
-echo Setting Initial Congestion Window size to 10 segments for InternetCustom profile.
+rem ::: Setting Initial Congestion Window size to 10 segments for InternetCustom profile.
 powershell -Command "Set-NetTCPSetting -SettingName InternetCustom -InitialCongestionWindow 10" >nul
 
-echo Setting TCP AutoTuningLevel to Normal and disabling ScalingHeuristics for InternetCustom profile.
+rem ::: Setting TCP AutoTuningLevel to Normal and disabling ScalingHeuristics for InternetCustom profile.
 powershell -Command "Set-NetTCPSetting -SettingName InternetCustom -AutoTuningLevelLocal Normal -ScalingHeuristics Disabled" >nul
 
 echo.
-
-echo Network Tweaks Script Completed...
 
 PAUSE
