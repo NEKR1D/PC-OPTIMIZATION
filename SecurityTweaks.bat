5fcfc5a282dac1c4b482011a8d3bd2fdb4800adf
L O G A N <154441675+NEKR1D@users.noreply.github.com>
2025-07-18 14:12:00 -0700

    Updated

--- SecurityTweaks.bat
+++ SecurityTweaks.bat
@@ -15,13 +15,14 @@ rem ::: !!! WARNING !!!
 
 PAUSE
 
-rem ::: 0/3 - Enable / 3/3 - Disable mitigations for CVE-2017-5715 (Spectre Variant 2) and CVE-2017-5754 (Meltdown)
-
+rem ::: CPU Mitigations for CVE-2017-5715 (Spectre Variant 2) and CVE-2017-5754 (Meltdown)
+rem ::: Enable = 0 / Disable = 3 
 reg add "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettingsOverride" /t REG_DWORD /d "3" /f
 reg add "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettingsOverrideMask" /t REG_DWORD /d "3" /f
 reg add "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettings" /t REG_DWORD /d "3" /f
 
-rem ::: 0 - Disable 1 = Enable - Windows Security Control Flow Guard
+rem ::: Windows Security Control Flow Guard
+rem ::: Disable = 0 Enable = 1
 rem ::: https://learn.microsoft.com/en-us/windows/win32/secbp/control-flow-guard
 reg add "HKLM\System\CurrentControlSet\Control\Session Manager\Memory Management" /v "EnableCfg" /t REG_DWORD /d "0" /f
 
