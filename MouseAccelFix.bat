af9fe76d24a6a830472934860a687c847c10ddae
L O G A N <154441675+NEKR1D@users.noreply.github.com>
2025-07-24 23:03:06 -0700

    Update MouseAccelFix.bat

--- MouseAccelFix.bat
+++ MouseAccelFix.bat
@@ -1,9 +1,12 @@
-rem ::: MarkC Mouse Acceleration Fix
+rem ::: Mouse Acceleration Fix
 
 rem ::: Plundered by NEKR1D
 
+rem ::: Originally created by MarkC
+rem ::: https://donewmouseaccel.blogspot.com/2010/03/markc-windows-7-mouse-acceleration-fix.html
+
 rem ::: !!! WARNING !!!
-rem ::: Windows scaling size must be set to 100%
+rem ::: !!! Windows scaling size must be set to 100% !!!
 
 PAUSE
 
@@ -24,12 +27,14 @@ reg add "HKU\.DEFAULT\Control Panel\Mouse" /f /v "SmoothMouseXCurve" /t REG_BINA
 reg add "HKU\.DEFAULT\Control Panel\Mouse" /f /v "SmoothMouseYCurve" /t REG_BINARY /d 0000000000000000000038000000000000007000000000000000A800000000000000E00000000000
 
 rem ::: MouseSensitivity (10 = Normal, 20 = Max)
+rem ::: Set this to 3 due to high DPI of 3200+, does not influence game engines but sometimes game menus
 reg add "HKU\.DEFAULT\Control Panel\Mouse" /f /v "MouseSensitivity" /t REG_SZ /d "3"
 reg add "HKCU\Control Panel\Mouse" /v "MouseSensitivity" /t REG_SZ /d "3" /f
 
 rem ::: Enable RAW Input
-reg add "HKCU\Control Panel\Mouse" /v RawInput /t REG_SZ /d 1 /f
+reg add "HKCU\Control Panel\Mouse" /v RawInput /t REG_SZ /d "1" /f
 
-reg add "HKCU\Control Panel\Mouse" /v ActiveWindowTracking /t REG_DWORD /d 0 /f
+rem ::: Activate a window by hovering over it with the mouse // allows hover scrolling
+reg add "HKCU\Control Panel\Mouse" /v ActiveWindowTracking /t REG_DWORD /d "1" /f
 
 PAUSE
