bf67bcddbbea14acd5d33d0940489d569a8621af
L O G A N <154441675+NEKR1D@users.noreply.github.com>
2025-07-29 18:07:37 -0700

    Update GPUTweaks.bat

--- GPUTweaks.bat
+++ GPUTweaks.bat
@@ -30,9 +30,10 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Enum\%%i\Device Parameters\Interrupt Mana
 rem ::: Enable MSI Mode for GPU
 rem ::: Device Manager > Display Adapters > "GPUNAME" > Properties > Details > Property > Device instant path
 rem ::: HKLM\SYSTEM\CurrentControlSet\Enum\PCI\"Device instant path"
+rem ::: Forces MSI Mode ON and Forces High Priority
 for /f %%g in ('wmic path win32_videocontroller get PNPDeviceID ^| findstr /L "VEN_"') do (
 reg add "HKLM\SYSTEM\CurrentControlSet\Enum\%%g\Device Parameters\Interrupt Management\MessageSignaledInterruptProperties" /v "MSISupported" /t REG_DWORD /d "1" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Enum\%%g\Device Parameters\Interrupt Management\Affinity Policy" /v "DevicePriority" /t REG_DWORD /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Enum\%%g\Device Parameters\Interrupt Management\Affinity Policy" /v "DevicePriority" /t REG_DWORD /d "3" /f
 )
 
 rem ::: Tell Windows to stop tolerating high DPC/ISR latencies
